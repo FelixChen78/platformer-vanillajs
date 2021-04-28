dfff20711821e5021465a85db14e8e06f760adfa
FelixChen78 <fchen33@buffalo.edu>
2021-04-28 15:50:13 -0400

    updated movement

--- demo-game/script.js
+++ demo-game/script.js
@@ -26,13 +26,6 @@ const player = {
     mass: 1,
 };
 
-const world = {
-    gravity: -9.8,
-    staticObjects: [],
-    dynamicObjects: [],
-    players: [],
-}
-
 const playerSprite = new Image();
 playerSprite.src = "assets/chewie.png";
 
@@ -68,29 +61,35 @@ window.addEventListener("keyup", e => {
 });
 
 function movePlayer() {
-    if ((keys["ArrowUp"] || keys[" "]) && player.y >= 0) {
-        player.y -= player.velocity;
+    if ((keys["ArrowUp"] || keys[" "])) {
+        if (player.y > 0) {
+            player.y -= player.velocity;
+        }
         player.frameY = 3;
         player.moving = true;
-
     }
-    if (keys["ArrowDown"] && player.y <= ground) {
-        player.y += player.velocity;
+    if (keys["ArrowDown"]) {
+        if (player.y < ground) {
+            player.y += player.velocity;
+        }
         player.frameY = 0;
         player.moving = true;
     }
-    if (keys["ArrowLeft"] && player.x >= 0) {
-        player.x -= player.velocity;
+    if (keys["ArrowLeft"]) {
+        if (player.x > 0) {
+            player.x -= player.velocity;
+        }
         player.frameY = 1;
         player.moving = true;
     }
-    if (keys["ArrowRight"] && player.x <= canvas.width - player.width) {
-        player.x += player.velocity
+    if (keys["ArrowRight"]) {
+        if (player.x < canvas.width - player.width) {
+            player.x += player.velocity
+        }
         player.frameY = 2;
         player.moving = true;
     }
 }
-
 
 /**                         ANIMATION                          */
 
@@ -111,26 +110,18 @@ function animate() {
     now = Date.now();
     elapsed = now - then;
 
-
     if (elapsed > fpsInterval) {
         then = now - (elapsed % fpsInterval);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height + 90);
-
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
-
 
         movePlayer(); //key movement
 
-
         handlePlayerFrame();
-
         requestAnimationFrame(animate);
-
     }
-
 }
 
 fps = 10;
-
 gameLoop(fps);
